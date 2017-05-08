a3cfbfe5883bb7d8b981bb5f913f137d1205d9c8
Anjali <anjaligr@usc.edu>
2017-05-08 08:51:21 -0700

    Fixing import exceptions

--- pipeline_work.py
+++ pipeline_work.py
@@ -1,6 +1,6 @@
 """
 Usage: 
-python pipeline_anjali_work.py -f SUB_190011/func/rest.nii
+python pipeline__work.py -f SUB_190011/func/rest.nii
       -t template_MNI152_2mm.nii.gz --TR 2 -s SUB_190011
       --subjects_dir fsdata --slice_times 0 17 1 18 2 19 3 20 4 21 5 22 6 23
       7 24 8 25 9 26 10 27 11 28 12 29 13 30 14 31 15 32 16 -o .
@@ -11,6 +11,15 @@ from __future__ import division, unicode_literals
 from builtins import open, range, str
 
 import os
+from nipype.interfaces import (spm, fsl, Function, ants, freesurfer)
+from nipype.interfaces.c3 import C3dAffineTool
+
+fsl.FSLCommand.set_default_output_type('NIFTI')
+
+from nipype import Workflow, Node, MapNode
+from nipype.interfaces import matlab as mlab
+
+mlab.MatlabCommand.set_default_matlab_cmd("matlab -nodisplay")
 
 from nipype.interfaces.base import CommandLine
 CommandLine.set_default_terminal_output("allatonce")
@@ -82,8 +91,226 @@ def anat_preproc(filename):
 output args: returns a 3d nifti file'''
 def avg_median(filename_in):
 	from nilearn.image import load_img
-""" Create the main preprocessing workflow """
+def create_reg_workflow(name='registration'):
+    """Create a FEAT preprocessing workflow together with freesurfer
+    Parameters
+    ----------
+        name : name of workflow (default: 'registration')
+    Inputs::
+        inputspec.source_files : files (filename or list of filenames to register)
+        inputspec.mean_image : reference image to use
+        inputspec.anatomical_image : anatomical image to coregister to
+        inputspec.target_image : registration target
+    Outputs::
+        outputspec.func2anat_transform : FLIRT transform
+        outputspec.anat2target_transform : FLIRT+FNIRT transform
+        outputspec.transformed_files : transformed files in target space
+        outputspec.transformed_mean : mean image in target space
+    """
 
+    register = Workflow(name=name)
+
+    inputnode = Node(interface=IdentityInterface(fields=['source_files',
+                                                         'mean_image',
+                                                         'subject_id',
+                                                         'subjects_dir',
+                                                         'target_image']),
+                     name='inputspec')
+
+    outputnode = Node(interface=IdentityInterface(fields=['func2anat_transform',
+                                                          'out_reg_file',
+                                                          'anat2target_transform',
+                                                          'transforms',
+                                                          'transformed_mean',
+                                                          'segmentation_files',
+                                                          'anat2target',
+                                                          'aparc'
+                                                          ]),
+                      name='outputspec')
+
+    # Get the subject's freesurfer source directory
+    fssource = Node(FreeSurferSource(),
+                    name='fssource')
+    fssource.run_without_submitting = True
+    register.connect(inputnode, 'subject_id', fssource, 'subject_id')
+    register.connect(inputnode, 'subjects_dir', fssource, 'subjects_dir')
+
+    convert = Node(freesurfer.MRIConvert(out_type='nii'),
+                   name="convert")
+    register.connect(fssource, 'T1', convert, 'in_file')
+
+    # Coregister the median to the surface
+    bbregister = Node(freesurfer.BBRegister(),
+                      name='bbregister')
+    bbregister.inputs.init = 'fsl'
+    bbregister.inputs.contrast_type = 't2'
+    bbregister.inputs.out_fsl_file = True
+    bbregister.inputs.epi_mask = True
+    register.connect(inputnode, 'subject_id', bbregister, 'subject_id')
+    register.connect(inputnode, 'mean_image', bbregister, 'source_file')
+    register.connect(inputnode, 'subjects_dir', bbregister, 'subjects_dir')
+
+    """
+    Estimate the tissue classes from the anatomical image. But use spm's segment
+    as FSL appears to be breaking.
+    """
+
+    stripper = Node(fsl.BET(), name='stripper')
+    register.connect(convert, 'out_file', stripper, 'in_file')
+    fast = Node(fsl.FAST(), name='fast')
+    register.connect(stripper, 'out_file', fast, 'in_files')
+
+    """
+    Binarize the segmentation
+    """
+
+    binarize = MapNode(fsl.ImageMaths(op_string='-nan -thr 0.9 -ero -bin'),
+                       iterfield=['in_file'],
+                       name='binarize')
+    register.connect(fast, 'partial_volume_files', binarize, 'in_file')
+
+    """
+    Apply inverse transform to take segmentations to functional space
+    """
+
+    applyxfm = MapNode(freesurfer.ApplyVolTransform(inverse=True,
+                                                    interp='nearest'),
+                       iterfield=['target_file'],
+                       name='inverse_transform')
+    register.connect(inputnode, 'subjects_dir', applyxfm, 'subjects_dir')
+    register.connect(bbregister, 'out_reg_file', applyxfm, 'reg_file')
+    register.connect(binarize, 'out_file', applyxfm, 'target_file')
+    register.connect(inputnode, 'mean_image', applyxfm, 'source_file')
+
+    """
+    Apply inverse transform to aparc file
+    """
+
+    aparcxfm = Node(freesurfer.ApplyVolTransform(inverse=True,
+                                                 interp='nearest'),
+                    name='aparc_inverse_transform')
+    register.connect(inputnode, 'subjects_dir', aparcxfm, 'subjects_dir')
+    register.connect(bbregister, 'out_reg_file', aparcxfm, 'reg_file')
+    register.connect(fssource, ('aparc_aseg', get_aparc_aseg),
+                     aparcxfm, 'target_file')
+    register.connect(inputnode, 'mean_image', aparcxfm, 'source_file')
+
+    """
+    Convert the BBRegister transformation to ANTS ITK format
+    """
+
+    convert2itk = Node(C3dAffineTool(), name='convert2itk')
+    convert2itk.inputs.fsl2ras = True
+    convert2itk.inputs.itk_transform = True
+    register.connect(bbregister, 'out_fsl_file', convert2itk, 'transform_file')
+    register.connect(inputnode, 'mean_image', convert2itk, 'source_file')
+    register.connect(stripper, 'out_file', convert2itk, 'reference_file')
+
+    """
+    Compute registration between the subject's structural and MNI template
+    This is currently set to perform a very quick registration. However, the
+    registration can be made significantly more accurate for cortical
+    structures by increasing the number of iterations
+    All parameters are set using the example from:
+    #https://github.com/stnava/ANTs/blob/master/Scripts/newAntsExample.sh
+    """
+
+    reg = Node(ants.Registration(), name='antsRegister')
+    reg.inputs.output_transform_prefix = "output_"
+    reg.inputs.transforms = ['Rigid', 'Affine', 'SyN']
+    reg.inputs.transform_parameters = [(0.1,), (0.1,), (0.2, 3.0, 0.0)]
+    reg.inputs.number_of_iterations = [[10000, 11110, 11110]] * 2 + [[100, 30, 20]]
+    reg.inputs.dimension = 3
+    reg.inputs.write_composite_transform = True
+    reg.inputs.collapse_output_transforms = True
+    reg.inputs.initial_moving_transform_com = True
+    reg.inputs.metric = ['Mattes'] * 2 + [['Mattes', 'CC']]
+    reg.inputs.metric_weight = [1] * 2 + [[0.5, 0.5]]
+    reg.inputs.radius_or_number_of_bins = [32] * 2 + [[32, 4]]
+    reg.inputs.sampling_strategy = ['Regular'] * 2 + [[None, None]]
+    reg.inputs.sampling_percentage = [0.3] * 2 + [[None, None]]
+    reg.inputs.convergence_threshold = [1.e-8] * 2 + [-0.01]
+    reg.inputs.convergence_window_size = [20] * 2 + [5]
+    reg.inputs.smoothing_sigmas = [[4, 2, 1]] * 2 + [[1, 0.5, 0]]
+    reg.inputs.sigma_units = ['vox'] * 3
+    reg.inputs.shrink_factors = [[3, 2, 1]] * 2 + [[4, 2, 1]]
+    reg.inputs.use_estimate_learning_rate_once = [True] * 3
+    reg.inputs.use_histogram_matching = [False] * 2 + [True]
+    reg.inputs.winsorize_lower_quantile = 0.005
+    reg.inputs.winsorize_upper_quantile = 0.995
+    reg.inputs.float = True
+    reg.inputs.output_warped_image = 'output_warped_image.nii.gz'
+    reg.inputs.num_threads = 4
+    reg.plugin_args = {'qsub_args': '-l nodes=1:ppn=4'}
+    register.connect(stripper, 'out_file', reg, 'moving_image')
+    register.connect(inputnode, 'target_image', reg, 'fixed_image')
+
+    """
+    Concatenate the affine and ants transforms into a list
+    """
+
+    merge = Node(Merge(2), iterfield=['in2'], name='mergexfm')
+    register.connect(convert2itk, 'itk_transform', merge, 'in2')
+    register.connect(reg, 'composite_transform', merge, 'in1')
+
+    """
+    Transform the mean image. First to anatomical and then to target
+    """
+
+    warpmean = Node(ants.ApplyTransforms(), name='warpmean')
+    warpmean.inputs.input_image_type = 3
+    warpmean.inputs.interpolation = 'Linear'
+    warpmean.inputs.invert_transform_flags = [False, False]
+    warpmean.inputs.terminal_output = 'file'
+    warpmean.inputs.args = '--float'
+    warpmean.inputs.num_threads = 4
+
+    register.connect(inputnode, 'target_image', warpmean, 'reference_image')
+    register.connect(inputnode, 'mean_image', warpmean, 'input_image')
+    register.connect(merge, 'out', warpmean, 'transforms')
+
+    """
+    Assign all the output files
+    """
+
+    register.connect(reg, 'warped_image', outputnode, 'anat2target')
+    register.connect(warpmean, 'output_image', outputnode, 'transformed_mean')
+    register.connect(applyxfm, 'transformed_file',
+                     outputnode, 'segmentation_files')
+    register.connect(aparcxfm, 'transformed_file',
+                     outputnode, 'aparc')
+    register.connect(bbregister, 'out_fsl_file',
+                     outputnode, 'func2anat_transform')
+    register.connect(bbregister, 'out_reg_file',
+                     outputnode, 'out_reg_file')
+    register.connect(reg, 'composite_transform',
+                     outputnode, 'anat2target_transform')
+    register.connect(merge, 'out', outputnode, 'transforms')
+
+    return register
+
+""" Create the main preprocessing workflow """
+def create_resting_workflow(args, name=None):
+    TR = args.TR
+    slice_times = args.slice_times
+    if name is None:
+        name = 'resting_' + args.subject_id
+    kwargs = dict(files=[os.path.abspath(filename) for filename in args.files],
+                  target_file=os.path.abspath(args.target_file),
+                  subject_id=args.subject_id,
+                  TR=TR,
+                  slice_times=slice_times,
+                  vol_fwhm=args.vol_fwhm,
+                  surf_fwhm=args.surf_fwhm,
+                  norm_threshold=2.,
+                  subjects_dir=os.path.abspath(args.fsdir),
+                  target_subject=args.target_surfs,
+                  lowpass_freq=args.lowpass_freq,
+                  highpass_freq=args.highpass_freq,
+                  sink_directory=os.path.abspath(args.sink),
+                  name=name)
+    wf = create_workflow(**kwargs)
+    return wf
 def create_workflow(files, target_file, subject_id, TR, slice_times, norm_threshold=1, num_components=5, vol_fwhm=None, surf_fwhm=None, lowpass_freq=-1, highpass_freq=-1, subjects_dir=None, sink_directory=os.getcwd(), target_subject=['fsaverage3','fsaverage4'],name='resting'):
 	'''create workflow object, with arg name passed'''
 	wf=Workflow(name=name)
@@ -113,9 +340,9 @@ def create_workflow(files, target_file, subject_id, TR, slice_times, norm_thresh
     	# Computing the median image across runs
     	calc_median = Node(Function(input_names=['in_files'],
                                 output_names=['median_file'],
-                                function=median,
+                                function=avg_median,
                                 imports=imports),
-                       name='median')
+                       name='avg_median')
     	wf.connect(tsnr, 'detrended_file', calc_median, 'in_files')
 
     	"""Segment and De register"""
@@ -435,15 +662,13 @@ if __name__ == "__main__":
     defstr = ' (default %(default)s)'
     parser = ArgumentParser(description=__doc__,
                             formatter_class=RawTextHelpFormatter)
-    parser.add_argument("-d", "--dicom_file", dest="dicom_file",
-                        help="an example dicom file from the resting series")
     parser.add_argument("-f", "--files", dest="files", nargs="+",
                         help="4d nifti files for resting state",
                         required=True)
     parser.add_argument("-t", "--target", dest="target_file",
                         help=("Target in MNI space. Best to use the MindBoggle "
                               "template - "
-                              "OASIS-30_Atropos_template_in_MNI152_2mm.nii.gz"),
+                              "Template_in_MNI152_2mm.nii.gz"),
                         required=True)
     parser.add_argument("-s", "--subject_id", dest="subject_id",
                         help="FreeSurfer subject id", required=True)
